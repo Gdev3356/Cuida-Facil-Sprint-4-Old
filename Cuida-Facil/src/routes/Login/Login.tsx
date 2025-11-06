3a1b43fcce51cd1ef48d794909f0ab9954cc5fdb
= <=>
2025-11-06 19:30:35 -0300

    feat: estilização de componentes de cadastro/login e status

--- Cuida-Facil/src/routes/Login/Login.tsx
+++ Cuida-Facil/src/routes/Login/Login.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import SistemaLogin from '../../components/Login/Login'; 
 import type { TipoPaciente } from '../../types';
+import Cabecalho from '../../components/Voltar/Voltar';
 
 export default function LoginPage() {
     const navigate = useNavigate();
@@ -11,10 +12,12 @@ export default function LoginPage() {
         login(paciente); 
         navigate('/');
     };
-
     return (
+    <header>
+        <Cabecalho/>
         <SistemaLogin 
             onLoginSucesso={handleLoginSucesso} 
         />
+    </header> 
     );
 }
