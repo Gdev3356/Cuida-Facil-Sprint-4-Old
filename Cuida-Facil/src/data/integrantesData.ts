642871028de8fca15bac255f09c15d638db8895d
= <=>
2025-11-07 17:38:20 -0300

    feat: remoção de membro inativo

--- Cuida-Facil/src/data/integrantesData.ts
+++ Cuida-Facil/src/data/integrantesData.ts
@@ -2,14 +2,6 @@ import type { TipoIntegrante } from '../types/Integrante';
  
 export const integrantesData: TipoIntegrante[] = [
   {
-    nome: 'Gustavo Bardy Casimiro',
-    rm: 'RM566136',
-    turma: '1TDSPW',
-    imageUrl: '/assets/img/bardy.jpg',
-    githubUrl: 'https://github.com/Gustavo-Casimiro',
-    linkedinUrl: 'https://www.linkedin.com/in/gustavo-casimiro',
-  },
-  {
     nome: 'Gustavo Keiji Okada',
     rm: 'RM563428',
     turma: '1TDSPH',
